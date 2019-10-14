a6813ca8580c11a1703245381d62070b9d742abd
Omer <sever@gmail.com>
2019-10-14 23:47:06 +0300

    new class github

---
+++ src/main/java/com/luv2code/springboot/cruddemo/GitHubDemo.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.cruddemo;
+
+public class GitHubDemo {
+
+	private String git_name;
+
+	public String getGit_name() {
+		return git_name;
+	}
+
+	public void setGit_name(String git_name) {
+		this.git_name = git_name;
+	}
+}
